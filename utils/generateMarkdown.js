a69d35211108967a2bf4a8fcb34c6ad432d5da79
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-02-03 18:53:29 -0600

    progress on markdown back tick text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  
+  switch(license) {
+    case x:
+      // code block
+      break;
+    case y:
+      // code block
+      break;
+    default:
+      console.log(`License ${license} note found`);
+  }
+  
+  
+  
+  return `![${license}](${licenseImage})](${renderLicenseLink(license)})`  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderGitHub(contributor) {
+  return 'Hi Mom'
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -20,25 +38,32 @@ function generateMarkdown(data) {
 
   ${renderLicenseBadge(data.license)}
 
-  [Installation](http://www.google.fr/ "Installation Section")
-  [Usage](http://www.google.fr/ "Usage Section")
-  [License](http://www.google.fr/ "License Section")
-  [Contributor](http://www.google.fr/ "Contributor Section")
-  [Tests](http://www.google.fr/ "Tests Section")
-  [Questions](http://www.google.fr/ "Questions Section")
+  *[Installation](#Installation)  
+  *[Usage](#Usage)
+  *[License](#License)
+  *[Contributor](#Contributor)
+  *[Tests](#Tests)
+  *[Questions](#Questions)
 
-  ## Installation
+  ##Installation
 
-  ## Usage
+  ${data.installation}
 
-  ## License
+  ##Usage
 
-  ## Contributor
+  ${data.Usage}
 
-  ## Tests
+  ##License
 
-  ## Questions
+  ${renderLicenseSection(data.license)}
 
+  ##Contributor
+
+  ${renderGitHub(data.contributor)}
+
+  ##Tests
+
+  ##Questions
 `;
 }
 
