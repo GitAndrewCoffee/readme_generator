d81b25e94b00cd6b985433969b00d2cb371e22fc
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-02-04 15:17:24 -0600

    updated init functio n and require statements

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-// TODO: Include packages needed for this application
+const inquire = require('inquirer')
+const fs = require('fs')
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(, markdown.generateMarkdown(data), function(err) {
+        console.log(err);
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquire.prompt(questions)
+        .then(data =>{
+            writeToFile('readme.md', data);
+        });
+}
 
 // Function call to initialize app
 init();
